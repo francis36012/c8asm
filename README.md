3a117671d989bf68802ec35ed37244d72354be5e
Francis Agayapong <francisagyapong2@gmail.com>
2016-06-17 15:36:29 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# c8asm
+c8asm is a Chip-8 assembler.
+
+## Building
+c8asm is written in rust and can be built using cargo:
+
+```shell
+cargo build --release
+```
+
+## Running
---
+++ src/main.rs
@@ -0,0 +1,36 @@
+enum Mnemonic {
+    Add, Call, Cls, Drw,
+    Jp, Ld, Or, Ret,
+    Rnd, Se, Shl, Shr,
+    Sknp, Skp, Sne, Sub,
+    Subn, Sys, Xor,
+}
+
+enum Register {
+    V0, V1, V2, V3,
+    V4, V5, V6, V7,
+    V8, V9, Va, Vb,
+    Vc, Vd, Ve, Vf,
+    Sp, Pc, I, St, Dt,
+}
+
+enum Token {
+    Opcode(Mnemonic),
+    Reg(Register),
+    ImmAddr(u16),
+    ImmByte(u8),
+    ImmNibb(u8),
+}
+
+struct Stream {
+}
+
+impl Stream {
+    fn next_token(&mut self) -> Option<Token> {
+        None
+    }
+}
+
+fn main() {
+    println!("Hello, world!");
+}
