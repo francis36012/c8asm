b1c4e9aaa98936e950aed3a6bb96dab742bd9e1c
Francis Agayapong <francisagyapong2@gmail.com>
2016-06-18 20:19:03 -0600

    Update
    * Replace all numeric variants in Token with a single variant of u16 (ImmConst(u16))
    * Add methods to Stream for testing if a string is numeric

--- src/main.rs
+++ src/main.rs
@@ -21,9 +21,7 @@ enum Register {
 enum Token {
     Opcode(Mnemonic),
     Reg(Register),
-    ImmAddr(u16),
-    ImmByte(u8),
-    ImmNibb(u8),
+    ImmConst(u16),
 }
 
 struct Stream {
@@ -61,7 +59,37 @@ impl Stream {
     }
 
     fn create_token(input: Vec<u8>) -> Option<Token> {
-        None
+        let token_str: String = String::from_utf8(input).unwrap_or_else(|err| {
+            println!("Error: {:?}", err);
+            "".to_owned()
+        }).to_lowercase();
+
+        if Stream::is_numeric(&token_str) {
+            return Some(Token::ImmConst(token_str.parse::<u16>().unwrap()));
+        }
+
+        match token_str.as_str() {
+            "add"   => Some(Token::Opcode(Mnemonic::Add)),
+            "call"  => Some(Token::Opcode(Mnemonic::Call)),
+            "cls"   => Some(Token::Opcode(Mnemonic::Cls)),
+            "drw"   => Some(Token::Opcode(Mnemonic::Drw)),
+            "jp"    => Some(Token::Opcode(Mnemonic::Jp)),
+            "ld"    => Some(Token::Opcode(Mnemonic::Ld)),
+            "or"    => Some(Token::Opcode(Mnemonic::Or)),
+            "ret"   => Some(Token::Opcode(Mnemonic::Ret)),
+            "rnd"   => Some(Token::Opcode(Mnemonic::Rnd)),
+            "se"    => Some(Token::Opcode(Mnemonic::Se)),
+            "shl"   => Some(Token::Opcode(Mnemonic::Shl)),
+            "shr"   => Some(Token::Opcode(Mnemonic::Shr)),
+            "sknp"  => Some(Token::Opcode(Mnemonic::Sknp)),
+            "skp"   => Some(Token::Opcode(Mnemonic::Skp)),
+            "sne"   => Some(Token::Opcode(Mnemonic::Sne)),
+            "sub"   => Some(Token::Opcode(Mnemonic::Sub)),
+            "subn"  => Some(Token::Opcode(Mnemonic::Subn)),
+            "sys"   => Some(Token::Opcode(Mnemonic::Sys)),
+            "xor"   => Some(Token::Opcode(Mnemonic::Xor)),
+            _       => None
+        }
     }
 
     fn is_separator(b: u8) -> bool {
@@ -69,6 +97,43 @@ impl Stream {
             ','|' '|'\n' => { true }
             _ => { false }
         }
+    }
+
+    fn is_numeric(input: &str) -> bool {
+        if input.len() < 1 {
+            return false;
+        }
+
+        let in_bytes = input.as_bytes();
+        if !Stream::is_ascii_numeric(in_bytes[0]) {
+            return false;
+        }
+
+        // hex
+        if input.starts_with("0x") {
+            for i in 2..(in_bytes.len()) {
+                if !Stream::is_ascii_hex(in_bytes[i]) {
+                    return false
+                }
+                return true
+            }
+        } else {
+            for b in in_bytes {
+                if !Stream::is_ascii_numeric(*b) {
+                    return false
+                }
+                return true
+            }
+        }
+        false
+    }
+
+    fn is_ascii_numeric(input: u8) -> bool {
+        input >= 48 && input <= 57
+    }
+
+    fn is_ascii_hex(input: u8) -> bool {
+        (input >= 48 && input <= 57) || ((input >= 97 && input <= 102) || (input >= 65 && input <= 70))
     }
 }
 
