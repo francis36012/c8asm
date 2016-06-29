c93dda75e89ad028755d9f9f7ccbb5be93a71345
Francis Agayapong <francisagyapong2@gmail.com>
2016-06-29 14:29:30 -0600

    Complete CLI

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
 #[macro_use]
 extern crate clap;
+extern crate c8asm;
 
 use std::path::Path;
 use std::fs::File;
+use std::io::Read;
+use std::io::Write;
 
 use clap::{Arg, App};
+
+use c8asm::lexer::Stream;
 
 fn main() {
     let matches = App::new("c8asm")
@@ -29,5 +34,32 @@ fn main() {
     let output_file_path = Path::new(matches.value_of("output").unwrap());
 
     let input_file = File::open(input_file_path).unwrap();
-    let output_file = File::open(output_file_path).unwrap();
+
+    let mut stream = Stream::new(input_file.bytes());
+    let mut tokens = vec![];
+
+    loop {
+        let maybe_token = stream.next_token();
+        if maybe_token.is_none() {
+            break;
+        }
+        let token = maybe_token.unwrap();
+        tokens.push(token);
+    }
+
+    let code = self::c8asm::lexer::code_gen(&tokens);
+    match code {
+        Ok(c) => {
+            let mut output_file = File::create(output_file_path).unwrap();
+            for i in c {
+                let buf = [((i & 0xff00) >> 8) as u8, (i & 0x00ff) as u8];
+                output_file.write(&buf);
+            }
+            std::process::exit(0);
+        },
+        Err(t) => {
+            println!("error: unexpected token: {:?}", t);
+            std::process::exit(1);
+        }
+    }
 }
